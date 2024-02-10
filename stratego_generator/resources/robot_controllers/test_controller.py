dd1add2af5eb27ae93ea29421045884d1320b6ad
livialestingi <livia.lestingi@polimi.it>
2024-02-10 22:08:53 +0100

    setting up structure

--- stratego_generator/resources/robot_controllers/test_controller.py
+++ stratego_generator/resources/robot_controllers/test_controller.py
@@ -1,8 +1,15 @@
 import sys
 
-from abm_runner import PROJECT_DIRECTORY
+import numpy as np
+
+import analyser
+from controller import AutonomicManagerController
+
+manager = AutonomicManagerController(analyser.SyntheticTypeAnalyser(model_file="/Users/lestingi/Desktop/phd-workspace/SaR/formal-robot-assisted-evacuation/stratego_generator/submodules/gi_estimator/model/trained_model.h5"))
+sample_sensor_reading = np.zeros(shape=(1, 30))  # type: np.ndarray
+gi_prob = manager.get_shared_identity_probability(sample_sensor_reading)
 
 if sys.argv[1] == '1':
-    print(PROJECT_DIRECTORY)
+    print('do-help')
 else:
     print('call-staff')
