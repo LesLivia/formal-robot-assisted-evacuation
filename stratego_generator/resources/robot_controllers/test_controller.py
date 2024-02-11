14ed651237ccd344adb8c9f7247879b21750dc46
livialestingi <livia.lestingi@polimi.it>
2024-02-11 14:19:14 +0100

    fixing relative paths

--- stratego_generator/resources/robot_controllers/test_controller.py
+++ stratego_generator/resources/robot_controllers/test_controller.py
@@ -1,13 +1,30 @@
+import os
 import sys
 
 import numpy as np
 
-import analyser
+curr_path = os.getcwd()
+SUBMODULE_PATH = 'submodules/gi_estimator'
+PROJECT_PATH = 'stratego_generator/' + SUBMODULE_PATH
+
+if 'impact' in curr_path:
+    sub_path = os.getcwd().replace('impact2.10.7', PROJECT_PATH)
+else:
+    sub_path = os.getcwd().replace('resources/robot_controllers', SUBMODULE_PATH)
+
+# necessary to access 'controller' in NetLogo
+sys.path.extend([sub_path])
+
+# necessary to suppress tensorflow logs
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+
 from controller import AutonomicManagerController
 
-manager = AutonomicManagerController(analyser.SyntheticTypeAnalyser(model_file="/Users/lestingi/Desktop/phd-workspace/SaR/formal-robot-assisted-evacuation/stratego_generator/submodules/gi_estimator/model/trained_model.h5"))
+manager = AutonomicManagerController()
 sample_sensor_reading = np.zeros(shape=(1, 30))  # type: np.ndarray
 gi_prob = manager.get_shared_identity_probability(sample_sensor_reading)
+
+print(gi_prob)
 
 if sys.argv[1] == '1':
     print('do-help')
