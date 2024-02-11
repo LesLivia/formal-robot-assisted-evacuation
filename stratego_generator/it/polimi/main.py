984b38269e2a95340f9c3912c9660f64f79b6ad1
livialestingi <livia.lestingi@polimi.it>
2024-02-11 17:07:47 +0100

    integrated stratego json parsing into strategy

--- stratego_generator/it/polimi/main.py
+++ stratego_generator/it/polimi/main.py
@@ -3,6 +3,7 @@ from it.polimi.mgrs.param_mgr import parse_params
 from it.polimi.mgrs.model_mgr import generate_model
 from it.polimi.mgrs.strategy_mgr import generate_strategy, parse_strategy
 from it.polimi.mgrs.code_mgr import generate_code
+from src.strategyviz.strategy2pta.opt_strategy import OptimizedStrategy
 
 LOGGER = Logger('main')
 
@@ -20,7 +21,7 @@ generate_strategy()
 
 LOGGER.info('Parsing Strategy...')
 
-parse_strategy()
+parsed_strategy: OptimizedStrategy = parse_strategy()
 
 LOGGER.info('Generating Decision-making Script...')
 
--- stratego_generator/it/polimi/mgrs/strategy_mgr.py
+++ stratego_generator/it/polimi/mgrs/strategy_mgr.py
@@ -1,6 +1,7 @@
 import configparser
 
 from it.polimi.logger.logger import Logger
+from src.strategyviz.strategy2pta.stratego_parser import parse_optimized_strategy
 
 LOGGER = Logger('Strategy Manager')
 
@@ -8,10 +9,17 @@ config = configparser.ConfigParser()
 config.read('./resources/config/config.ini')
 config.sections()
 
+STRATEGY_NAME = config['STRATEGY SETTINGS']['STRATEGY_NAME']
+STRATEGY_PATH = config['STRATEGY SETTINGS']['STRATEGY_PATH'].format(STRATEGY_NAME)
+
 
 def generate_strategy():
     LOGGER.info(config['UPPAAL SETTINGS']['UPPAAL_SCRIPT_PATH'])
 
 
 def parse_strategy():
-    return
+    with open(STRATEGY_PATH) as opt_strategy_file:
+        data: str = opt_strategy_file.read()
+        optimized_strategy = parse_optimized_strategy(STRATEGY_NAME, data)
+
+    return optimized_strategy
