3d1d7e86401f02eee8c412475c293565b3100c7f
livialestingi <livia.lestingi@polimi.it>
2024-08-13 17:57:33 +0200

    added parameter design space exploration for multi_victim model

--- stratego_generator/it/polimi/upp_verification.py
+++ stratego_generator/it/polimi/upp_verification.py
@@ -16,10 +16,15 @@ config.read('./resources/config/config.ini')
 
 template_path = config['TEMPLATES SETTING']['TEMPLATES_PATH']
 
-# TODO: can be customisable
-N_V = 1
-N_S = 2
-N = -1
+with open('./resources/config/model_config.json') as model_config_file:
+    model_params = json.load(model_config_file)
+
+N_V = int(model_params['n_victims'])
+N_S = int(model_params['n_survivors'])
+if len(sys.argv) > 2:
+    N = sys.argv[2]
+else:
+    N = -1
 
 try:
     # Chooses which version of the model to use (either with history, multiple victims, or multiple survivors).
@@ -32,15 +37,20 @@ try:
 except IndexError:
     print("First argument either 'hist', 'multi_victim', or 'multi_survivor'.")
 else:
-    FIXED_PARAMS: Dict[str, int] = {'timebound': 20, 'rat_deg': 1.0, 'dist_fr': 15, 'speed': 1}
+    FIXED_PARAMS: Dict[str, int] = {'timebound': 12, 'rat_deg': 1.0, 'dist_fr': 15, 'speed': 1, 'dist_v': 0}
 
     # Processes parameters search space
-    with open('./resources/config/params.json') as json_file:
+    with open('./resources/config/model_params.json') as json_file:
         params_ss = json.load(json_file)
         design_params: List[Design_Parameter] = []
         for key in params_ss:
             if "S{}" in params_ss[key]["key"]:
                 for i in range(N_S):
+                    design_params.append(
+                        Design_Parameter(key, params_ss[key]["key"].format(i + 1), params_ss[key]["type"],
+                                         params_ss[key]["min"], params_ss[key]["max"]))
+            elif "V{}" in params_ss[key]["key"]:
+                for i in range(N_V):
                     design_params.append(
                         Design_Parameter(key, params_ss[key]["key"].format(i + 1), params_ss[key]["type"],
                                          params_ss[key]["min"], params_ss[key]["max"]))
@@ -55,17 +65,28 @@ else:
     pr_property_pre = []
     pr_property_post = []
 
+    # For each configuration --->
     for conf in tqdm(configurations[:N]):
         with open(template) as template_file:
             tplt_content = template_file.read()
             for i, param in enumerate(conf):
                 tplt_content = tplt_content.replace(design_params[i].key, str(param))
 
+            # FIXME: should be configurable
+            if model_name == 'sar_multi_victim.xml':
+                tplt_content = tplt_content.replace("**N_V**", str(N_V))
+                tplt_content = tplt_content.replace("**PI_S**",
+                                                    '{' + ','.join([str(x) for x in model_params['pi_survivors']['s1']])
+                                                    + '}')
+                tplt_content = tplt_content.replace("**PI_V**", '{' + ','.join(
+                    ['{' + ','.join([str(x) for x in model_params['pi_victims'][v]]) + '}' for v in
+                     model_params['pi_victims']]) + '}')
+
         new_model = config['TEMPLATES SETTING']['MODEL_PATH'].format(os.getcwd()) + model_name
         with open(new_model, 'w') as new_model_file:
             new_model_file.write(tplt_content)
 
-        # Runs verification with Uppaal
+        # ---> performs verification with Uppaal
         uppaal_path = config['UPPAAL SETTINGS']['UPPAAL_PATH']
         script_path = config['UPPAAL SETTINGS']['UPPAAL_SCRIPT_PATH'].format(os.getcwd())
         out_path = config['UPPAAL SETTINGS']['UPPAAL_OUT_PATH'].format(os.getcwd(), sys.argv[1])
@@ -73,8 +94,9 @@ else:
 
         with open(out_path) as out_file:
             lines = out_file.readlines()
-            EXP_VALUE = " -- Formula: E[<=TAU;10000](max: r_payoff)"
-            indexes = [i for i, line in enumerate(lines) if EXP_VALUE in line]
+            EXP_VALUE_PRE = " -- Formula: E[<=TAU;10000](max: r_payoff)\n"
+            EXP_VALUE_POST = " -- Formula: E[<=TAU;10000](max: r_payoff) under {}\n".format(model_params['strat_name'])
+            indexes = [i for i, line in enumerate(lines) if line in [EXP_VALUE_PRE, EXP_VALUE_POST]]
             est = []
             for i in indexes:
                 try:
@@ -82,42 +104,43 @@ else:
                 except ValueError:
                     est.append(float(lines[i + 2].split(' ')[-1]))
             try:
-                exp_values.append((est[1] - est[0]) / est[0])
+                exp_values.append((est[1] - est[0]) / est[0] * 100)
             except ZeroDivisionError:
                 exp_values.append(0)
 
-            PROPERTY = " -- Formula: Pr[<=TAU](<> s1.__end__ or s2.__end__)\n"
+            PROPERTY = " -- Formula: Pr[<=TAU](<> {})\n".format(model_params['property'])
             line = [i for i, l in enumerate(lines) if l == PROPERTY][0]
             pr = [float(x) for x in lines[line + 2].split('[')[1].split(']')[0].split(',')]
             pr_property_pre.append(pr[0] + (pr[1] - pr[0]) / 2)
 
-            PROPERTY = " -- Formula: Pr[<=TAU](<> s1.__end__ or s2.__end__) under EndOptR\n"
+            PROPERTY = " -- Formula: Pr[<=TAU](<> {}) under {}\n".format(model_params['property'],
+                                                                         model_params['strat_name'])
             line = [i for i, l in enumerate(lines) if l == PROPERTY][0]
             pr = [float(x) for x in lines[line + 2].split('[')[1].split(']')[0].split(',')]
             pr_property_post.append(pr[0] + (pr[1] - pr[0]) / 2)
 
-    fig, axs = plt.subplots(ncols=3, figsize=(15, 5))
+    fig, axs = plt.subplots(ncols=3, figsize=(20, 5))
 
-    data_1 = pd.DataFrame(data={'dist1': [conf[4] for conf in configurations[:N]],
-                                'dist2': [conf[5] for conf in configurations[:N]],
+    data_1 = pd.DataFrame(data={'dist1': [conf[model_params["plot_dim_1"]] for conf in configurations[:N]],
+                                'dist2': [conf[model_params["plot_dim_2"]] for conf in configurations[:N]],
                                 'pr': pr_property_pre})
     data_1 = data_1.pivot(index='dist2', columns='dist1', values='pr')
     sns.heatmap(data_1, cmap="Reds", ax=axs[0], vmin=0.0, vmax=1.0)
 
     axs[0].set_title('Pr. ending within {}'.format(FIXED_PARAMS['timebound']), fontsize=10)
 
-    data_2 = pd.DataFrame(data={'dist1': [conf[4] for conf in configurations[:N]],
-                                'dist2': [conf[5] for conf in configurations[:N]],
+    data_2 = pd.DataFrame(data={'dist1': [conf[model_params["plot_dim_1"]] for conf in configurations[:N]],
+                                'dist2': [conf[model_params["plot_dim_2"]] for conf in configurations[:N]],
                                 'gain': exp_values})
     data_2 = data_2.pivot(index='dist2', columns='dist1', values='gain')
-    sns.heatmap(data_2, cmap="Blues", ax=axs[1])
+    sns.heatmap(data_2, cmap="Blues", ax=axs[1], vmin=-100.0, vmax=100.0)
 
     axs[1].set_title('Covered dist. decrease under strategy\n'
                      '(rat.deg:{}, tb: {}, speeds:{})'.format(FIXED_PARAMS['rat_deg'], FIXED_PARAMS['timebound'],
                                                               FIXED_PARAMS['speed']), fontsize=10)
 
-    data_3 = pd.DataFrame(data={'dist1': [conf[4] for conf in configurations[:N]],
-                                'dist2': [conf[5] for conf in configurations[:N]],
+    data_3 = pd.DataFrame(data={'dist1': [conf[model_params["plot_dim_1"]] for conf in configurations[:N]],
+                                'dist2': [conf[model_params["plot_dim_2"]] for conf in configurations[:N]],
                                 'pr': pr_property_post})
     data_3 = data_3.pivot(index='dist2', columns='dist1', values='pr')
     sns.heatmap(data_3, cmap="Reds", ax=axs[2], vmin=0.0, vmax=1.0)
