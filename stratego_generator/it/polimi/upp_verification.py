e9d29aeea2da37203c9c06e8dc450ecdd4f9f792
livialestingi <livia.lestingi@polimi.it>
2024-08-15 20:47:24 +0200

    added results for with-memory configuration

--- stratego_generator/it/polimi/upp_verification.py
+++ stratego_generator/it/polimi/upp_verification.py
@@ -7,7 +7,6 @@ from bisect import bisect_left
 from typing import List, Dict
 
 import matplotlib.pyplot as plt
-import pandas as pd
 import scipy.stats as ss
 import seaborn as sns
 from tqdm import tqdm
@@ -47,7 +46,7 @@ with open('./resources/config/model_config.json') as model_config_file:
 N_V = int(model_params['n_victims'])
 N_S = int(model_params['n_survivors'])
 if len(sys.argv) > 2:
-    N = sys.argv[2]
+    N = int(sys.argv[2])
 else:
     N = -1
 
@@ -62,7 +61,7 @@ try:
 except IndexError:
     print("First argument either 'hist', 'multi_victim', or 'multi_survivor'.")
 else:
-    FIXED_PARAMS: Dict[str, int] = {'timebound': 15, 'rat_deg': 0.75, 'dist_fr': 15, 'speed': 1, 'dist_s': 0}
+    FIXED_PARAMS: Dict[str, int] = {'timebound': 30, 'rat_deg': 0.75, 'dist_fr': 15, 'speed': 1, 'dist_s': 0}
 
     # Processes parameters search space
     with open('./resources/config/model_params.json') as json_file:
@@ -100,11 +99,11 @@ else:
                 tplt_content = tplt_content.replace(design_params[i].key, str(param))
 
             # FIXME: should be configurable
-            if model_name == 'sar_multi_victim.xml':
+            if model_name in ['sar_multi_victim.xml', 'sar_hist.xml']:
                 tplt_content = tplt_content.replace("**N_V**", str(N_V))
-                tplt_content = tplt_content.replace("**PI_S**",
-                                                    '{' + ','.join([str(x) for x in model_params['pi_survivors']['s1']])
-                                                    + '}')
+                tplt_content = tplt_content.replace("**PI_S**", '{' + ','.join(
+                    ['{' + ','.join([str(x) for x in model_params['pi_survivors'][v]]) + '}' for v in
+                     model_params['pi_survivors']]) + '}')
                 tplt_content = tplt_content.replace("**PI_V**", '{' + ','.join(
                     ['{' + ','.join([str(x) for x in model_params['pi_victims'][v]]) + '}' for v in
                      model_params['pi_victims']]) + '}')
@@ -118,15 +117,14 @@ else:
         script_path = config['UPPAAL SETTINGS']['UPPAAL_SCRIPT_PATH'].format(os.getcwd())
         out_path = config['UPPAAL SETTINGS']['UPPAAL_OUT_PATH'].format(os.getcwd(),
                                                                        model_name.replace('.xml', '') +
-                                                                       '_' + str(FIXED_PARAMS['timebound']) +
-                                                                       '_' + str(conf[model_params["plot_dim_1"]]) +
-                                                                       '_' + str(conf[model_params["plot_dim_2"]]))
+                                                                       '_' + str(FIXED_PARAMS['timebound']) + '_' +
+                                                                       '_'.join([str(x) for x in conf[15:-2]]))
         os.system("{} {} {} {}".format(script_path, uppaal_path, new_model, out_path))
 
         with open(out_path) as out_file:
             lines = out_file.readlines()
-            EXP_VALUE_PRE = " -- Formula: E[<=TAU;10000](max: r_payoff)\n"
-            EXP_VALUE_POST = " -- Formula: E[<=TAU;10000](max: r_payoff) under {}\n".format(model_params['strat_name'])
+            EXP_VALUE_PRE = " -- Formula: E[<=TAU;10000](max: fr_usage)\n"
+            EXP_VALUE_POST = " -- Formula: E[<=TAU;10000](max: fr_usage) under {}\n".format(model_params['strat_name'])
             indexes = [i for i, line in enumerate(lines) if line in [EXP_VALUE_PRE, EXP_VALUE_POST]]
             est = []
             for i in indexes:
@@ -155,35 +153,35 @@ else:
             log_file.write('{},{},{},{}\n'.format(out_path.split('/')[-1], conf[model_params["plot_dim_1"]],
                                                   conf[model_params["plot_dim_2"]], time.time() - start_time))
 
-    fig, axs = plt.subplots(ncols=3, figsize=(20, 5))
-
-    data_1 = pd.DataFrame(data={'dist1': [conf[model_params["plot_dim_1"]] for conf in configurations[:N]],
-                                'dist2': [conf[model_params["plot_dim_2"]] for conf in configurations[:N]],
-                                'pr': pr_property_pre})
-    data_1 = data_1.pivot(index='dist2', columns='dist1', values='pr')
-    sns.heatmap(data_1, cmap="Reds", ax=axs[0], vmin=0.0, vmax=1.0)
-
-    axs[0].set_title('Pr. ending within {}'.format(FIXED_PARAMS['timebound']), fontsize=10)
-
-    data_2 = pd.DataFrame(data={'dist1': [conf[model_params["plot_dim_1"]] for conf in configurations[:N]],
-                                'dist2': [conf[model_params["plot_dim_2"]] for conf in configurations[:N]],
-                                'gain': [x[2] for x in exp_values]})
-    data_2 = data_2.pivot(index='dist2', columns='dist1', values='gain')
-    sns.heatmap(data_2, cmap="Blues", ax=axs[1], vmin=-100.0, vmax=100.0)
-
-    axs[1].set_title('Covered dist. decrease under strategy\n'
-                     '(rat.deg:{}, tb: {}, speeds:{})'.format(FIXED_PARAMS['rat_deg'], FIXED_PARAMS['timebound'],
-                                                              FIXED_PARAMS['speed']), fontsize=10)
-
-    data_3 = pd.DataFrame(data={'dist1': [conf[model_params["plot_dim_1"]] for conf in configurations[:N]],
-                                'dist2': [conf[model_params["plot_dim_2"]] for conf in configurations[:N]],
-                                'pr': pr_property_post})
-    data_3 = data_3.pivot(index='dist2', columns='dist1', values='pr')
-    sns.heatmap(data_3, cmap="Reds", ax=axs[2], vmin=0.0, vmax=1.0)
-
-    axs[2].set_title('Pr. ending within {} under strategy'.format(FIXED_PARAMS['timebound']), fontsize=10)
-
-    plt.show()
+    # fig, axs = plt.subplots(ncols=3, figsize=(20, 5))
+    #
+    # data_1 = pd.DataFrame(data={'dist1': [conf[model_params["plot_dim_1"]] for conf in configurations[:N]],
+    #                             'dist2': [conf[model_params["plot_dim_2"]] for conf in configurations[:N]],
+    #                             'pr': pr_property_pre})
+    # data_1 = data_1.pivot(index='dist2', columns='dist1', values='pr')
+    # sns.heatmap(data_1, cmap="Reds", ax=axs[0], vmin=0.0, vmax=1.0)
+    #
+    # axs[0].set_title('Pr. ending within {}'.format(FIXED_PARAMS['timebound']), fontsize=10)
+    #
+    # data_2 = pd.DataFrame(data={'dist1': [conf[model_params["plot_dim_1"]] for conf in configurations[:N]],
+    #                             'dist2': [conf[model_params["plot_dim_2"]] for conf in configurations[:N]],
+    #                             'gain': [x[2] for x in exp_values]})
+    # data_2 = data_2.pivot(index='dist2', columns='dist1', values='gain')
+    # sns.heatmap(data_2, cmap="Blues", ax=axs[1], vmin=-100.0, vmax=100.0)
+    #
+    # axs[1].set_title('Covered dist. decrease under strategy\n'
+    #                  '(rat.deg:{}, tb: {}, speeds:{})'.format(FIXED_PARAMS['rat_deg'], FIXED_PARAMS['timebound'],
+    #                                                           FIXED_PARAMS['speed']), fontsize=10)
+    #
+    # data_3 = pd.DataFrame(data={'dist1': [conf[model_params["plot_dim_1"]] for conf in configurations[:N]],
+    #                             'dist2': [conf[model_params["plot_dim_2"]] for conf in configurations[:N]],
+    #                             'pr': pr_property_post})
+    # data_3 = data_3.pivot(index='dist2', columns='dist1', values='pr')
+    # sns.heatmap(data_3, cmap="Reds", ax=axs[2], vmin=0.0, vmax=1.0)
+    #
+    # axs[2].set_title('Pr. ending within {} under strategy'.format(FIXED_PARAMS['timebound']), fontsize=10)
+    #
+    # plt.show()
 
     fig, axs = plt.subplots(ncols=2, figsize=(7, 3))
 
@@ -197,7 +195,7 @@ else:
 
     stat, pvalue = ss.mannwhitneyu(data_1[0], data_1[1])
     est, mag = VD_A(data_1[0], data_1[1])
-    axs[0].set_title('Exp. Value of Task Duration\np-value: {:.1e},\n'
+    axs[0].set_title('Exp. Value of FR Calls\np-value: {:.1e},\n'
                      'effect size: {}'.format(pvalue, mag), fontsize=12)
     axs[0].set_xticklabels(['BL', 'FormIdeAble'], fontsize=12)
 
