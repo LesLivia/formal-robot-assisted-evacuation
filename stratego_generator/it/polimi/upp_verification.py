2d5c79dac4b867e687826e6daaae71a1198c6769
livialestingi <livia.lestingi@polimi.it>
2024-08-15 16:55:37 +0200

    added statistical tests

--- stratego_generator/it/polimi/upp_verification.py
+++ stratego_generator/it/polimi/upp_verification.py
@@ -2,17 +2,42 @@ import configparser
 import json
 import os
 import sys
+import time
+from bisect import bisect_left
 from typing import List, Dict
 
 import matplotlib.pyplot as plt
 import pandas as pd
+import scipy.stats as ss
 import seaborn as sns
 from tqdm import tqdm
 
 from it.polimi.model.params import Design_Parameter, get_permutations
 
+
+def VD_A(treatment: List[float], control: List[float]):
+    m = len(treatment)
+    n = len(control)
+    # if m != n:
+    #    raise ValueError("Data d and f must have the same length")
+    r = ss.rankdata(treatment + control)
+    r1 = sum(r[0:m])
+    # Compute the measure
+    # A = (r1/m - (m+1)/2)/n # formula (14) in Vargha and Delaney, 2000
+    A = (2 * r1 - m * (m + 1)) / (2 * n * m)  # equivalent formula to avoid accuracy errors
+    levels = [0.147, 0.33, 0.474]  # effect sizes from Hess and Kromrey, 2004
+    magnitude = ["negligible", "small", "medium", "large"]
+    scaled_A = (A - 0.5) * 2
+    magnitude = magnitude[bisect_left(levels, abs(scaled_A))]
+    estimate = A
+    return estimate, magnitude
+
+
 config = configparser.ConfigParser()
 config.read('./resources/config/config.ini')
+
+with open(config['UPPAAL SETTINGS']['uppaal.time.log'].format(os.getcwd()), 'w') as log_file:
+    log_file.truncate(0)
 
 template_path = config['TEMPLATES SETTING']['TEMPLATES_PATH']
 
@@ -33,11 +58,11 @@ try:
     elif sys.argv[1] == 'multi_victim':
         model_name = 'sar_multi_victim.xml'
     elif sys.argv[1] == 'multi_survivor':
-        model_name = 'sar_multi_survivor.xml'
+        model_name = 'sar_pi_multi_survivor.xml'
 except IndexError:
     print("First argument either 'hist', 'multi_victim', or 'multi_survivor'.")
 else:
-    FIXED_PARAMS: Dict[str, int] = {'timebound': 12, 'rat_deg': 1.0, 'dist_fr': 15, 'speed': 1, 'dist_v': 0}
+    FIXED_PARAMS: Dict[str, int] = {'timebound': 18, 'rat_deg': 0.75, 'dist_fr': 15, 'speed': 1, 'dist_v': 0}
 
     # Processes parameters search space
     with open('./resources/config/model_params.json') as json_file:
@@ -67,6 +92,8 @@ else:
 
     # For each configuration --->
     for conf in tqdm(configurations[:N]):
+        start_time = time.time()
+
         with open(template) as template_file:
             tplt_content = template_file.read()
             for i, param in enumerate(conf):
@@ -89,7 +116,11 @@ else:
         # ---> performs verification with Uppaal
         uppaal_path = config['UPPAAL SETTINGS']['UPPAAL_PATH']
         script_path = config['UPPAAL SETTINGS']['UPPAAL_SCRIPT_PATH'].format(os.getcwd())
-        out_path = config['UPPAAL SETTINGS']['UPPAAL_OUT_PATH'].format(os.getcwd(), sys.argv[1])
+        out_path = config['UPPAAL SETTINGS']['UPPAAL_OUT_PATH'].format(os.getcwd(),
+                                                                       model_name.replace('.xml', '') +
+                                                                       '_' + str(FIXED_PARAMS['timebound']) +
+                                                                       '_' + str(conf[model_params["plot_dim_1"]]) +
+                                                                       '_' + str(conf[model_params["plot_dim_2"]]))
         os.system("{} {} {} {}".format(script_path, uppaal_path, new_model, out_path))
 
         with open(out_path) as out_file:
@@ -104,7 +135,7 @@ else:
                 except ValueError:
                     est.append(float(lines[i + 2].split(' ')[-1]))
             try:
-                exp_values.append((est[1] - est[0]) / est[0] * 100)
+                exp_values.append((est[0], est[1], (est[1] - est[0]) / est[0] * 100))
             except ZeroDivisionError:
                 exp_values.append(0)
 
@@ -119,6 +150,11 @@ else:
             pr = [float(x) for x in lines[line + 2].split('[')[1].split(']')[0].split(',')]
             pr_property_post.append(pr[0] + (pr[1] - pr[0]) / 2)
 
+        # RQ3: logs wall-clock time necessary to perform the verification
+        with open(config['UPPAAL SETTINGS']['uppaal.time.log'].format(os.getcwd()), 'a') as log_file:
+            log_file.write('{},{},{},{}\n'.format(out_path.split('/')[-1], conf[model_params["plot_dim_1"]],
+                                                  conf[model_params["plot_dim_2"]], time.time() - start_time))
+
     fig, axs = plt.subplots(ncols=3, figsize=(20, 5))
 
     data_1 = pd.DataFrame(data={'dist1': [conf[model_params["plot_dim_1"]] for conf in configurations[:N]],
@@ -131,7 +167,7 @@ else:
 
     data_2 = pd.DataFrame(data={'dist1': [conf[model_params["plot_dim_1"]] for conf in configurations[:N]],
                                 'dist2': [conf[model_params["plot_dim_2"]] for conf in configurations[:N]],
-                                'gain': exp_values})
+                                'gain': [x[2] for x in exp_values]})
     data_2 = data_2.pivot(index='dist2', columns='dist1', values='gain')
     sns.heatmap(data_2, cmap="Blues", ax=axs[1], vmin=-100.0, vmax=100.0)
 
@@ -147,4 +183,29 @@ else:
 
     axs[2].set_title('Pr. ending within {} under strategy'.format(FIXED_PARAMS['timebound']), fontsize=10)
 
+    plt.show()
+
+    fig, axs = plt.subplots(ncols=2, figsize=(7, 3))
+
+    data_1 = [[x[0] for x in exp_values], [x[1] for x in exp_values]]
+    sns.boxplot(data=data_1, ax=axs[0], showmeans=True,
+                meanprops={'marker': '^', 'markerfacecolor': 'white', 'markeredgecolor': 'black', 'markersize': '9'})
+
+    data_2 = [pr_property_pre, pr_property_post]
+    sns.boxplot(data=data_2, ax=axs[1], showmeans=True,
+                meanprops={'marker': '^', 'markerfacecolor': 'white', 'markeredgecolor': 'black', 'markersize': '9'})
+
+    stat, pvalue = ss.mannwhitneyu(data_1[0], data_1[1])
+    est, mag = VD_A(data_1[0], data_1[1])
+    axs[0].set_title('Exp. Value of Task Duration\np-value: {:.1e},\n'
+                     'effect size: {}'.format(pvalue, mag), fontsize=12)
+    axs[0].set_xticklabels(['BL', 'FormIdeAble'], fontsize=12)
+
+    stat, pvalue = ss.mannwhitneyu(data_2[0], data_2[1])
+    est, mag = VD_A(data_2[0], data_2[1])
+    axs[1].set_title('Pr. of psi holding\np-value: {:.1e},\n'
+                     'effect size: {}'.format(pvalue, mag), fontsize=12)
+    axs[1].set_xticklabels(['BL', 'FormIdeAble'], fontsize=12)
+
+    plt.savefig("rq1_box_{}_{}.pdf".format(sys.argv[1], FIXED_PARAMS['timebound']), bbox_inches="tight")
     plt.show()
